d011adeb55ea5171b63b20ea992602f7792bd8b4
Anton Ekström <anton.e.ekstrom@gmail.com>
2020-03-16 21:56:48 +0100

    YAAASSS QUEEEN

---
+++ Assets/Camera/FollowingCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 3, -100);
+    public float lerpSpeed = 3f;
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, lerpSpeed * Time.deltaTime);
+    }
+
+}
